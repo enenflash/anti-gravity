cdc3d768c53f5e8d308c42ff1cb5dd6e0b1bd614
Enenflash <34340998+enenflash@users.noreply.github.com>
2025-04-09 17:16:14 +0800

    sound class

---
+++ src/sound.py
@@ -0,0 +1,22 @@
+import pygame as pg
+from settings import *
+
+class Sound:
+    def __init__ (self):
+        pg.mixer.init()
+        self.path = 'resources/sound/'
+        
+        self.sounds = {
+            "quack": pg.mixer.Sound(self.path + 'quack.mp3'),
+            "victory": pg.mixer.Sound(self.path + 'victory.mp3')
+        }
+
+    def play_sound(self, name:str) -> None:
+        if VOLUME == 0:
+            return
+        if name not in self.sounds:
+            print(f"{name} not in sound library")
+            return
+        self.sounds[name].play()
+
+game_sound = Sound()
